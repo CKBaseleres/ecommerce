defd8e81e08bfcbe02bf823abec5f6b3873cc641
Carl Keneth Baseleres <baseleres.ck@gmail.com>
2019-08-31 15:29:38 +0800

    Migrated collections to firebase and adjusted redux to fetch from the backend

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,15 +1,15 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyA3Gpnwss-AUj2dD44fgRBCvcnuF1dmBu0",
-  authDomain: "crwn-db-3b3fd.firebaseapp.com",
-  databaseURL: "https://crwn-db-3b3fd.firebaseio.com",
-  projectId: "crwn-db-3b3fd",
-  storageBucket: "",
-  messagingSenderId: "403038253738",
-  appId: "1:403038253738:web:dff7217a64251887"
+  apiKey: 'AIzaSyA3Gpnwss-AUj2dD44fgRBCvcnuF1dmBu0',
+  authDomain: 'crwn-db-3b3fd.firebaseapp.com',
+  databaseURL: 'https://crwn-db-3b3fd.firebaseio.com',
+  projectId: 'crwn-db-3b3fd',
+  storageBucket: '',
+  messagingSenderId: '403038253738',
+  appId: '1:403038253738:web:dff7217a64251887'
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
@@ -31,7 +31,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         ...additionalData
       });
     } catch (error) {
-      console.log("error creating user", error.message);
+      console.log('error creating user', error.message);
     }
   }
 
@@ -40,10 +40,42 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
 firebase.initializeApp(config);
 
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnaphotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_account" });
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
