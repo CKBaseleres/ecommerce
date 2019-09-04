4b0d313b57b7845645af09ce38e789a52db63c73
Carl Keneth Baseleres <baseleres.ck@gmail.com>
2019-09-04 18:42:47 +0800

    Separating client app to a separate folder, added server app in root folder

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 import Logo from '../../assets/crown.svg';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_aSZmslrOMtrDbz08fxsAGQuo00mUGdZeMd';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(response => {
+        alert('Payment Successful');
+      })
+      .catch(error => {
+        console.log('Payment error: ', JSON.parse(error));
+        alert(
+          'There was an issue with your payment. Please make sure you use the provided credit card.'
+        );
+      });
   };
+
   return (
     <StripeCheckout
       label='Pay Now'
