401f6f4d950bffec6aaba745118440c591e9f17a
Carl Keneth Baseleres <baseleres.ck@gmail.com>
2019-09-05 20:46:00 +0800

    Added media queries for responsiveness

--- client/src/components/collection-item/collection-item.styles.js
+++ client/src/components/collection-item/collection-item.styles.js
@@ -17,6 +17,17 @@ export const CollectionItemContainer = styled.div`
       display: flex;
     }
   }
+  @media screen and (max-widht: 800px) {
+    width: 40vw;
+    &:hover {
+      .image {
+        opacity: unset;
+      }
+      button {
+        opacity: unset;
+      }
+    }
+  }
 `;
 
 export const AddButton = styled(CustomButton)`
@@ -25,6 +36,13 @@ export const AddButton = styled(CustomButton)`
   position: absolute;
   top: 255px;
   display: none;
+
+  @media screen and (max-width: 800px) {
+    display: block;
+    opacity: 0.9;
+    padding: 0 10px
+    min-width: unset;
+  }
 `;
 
 export const BackgroundImage = styled.div`
